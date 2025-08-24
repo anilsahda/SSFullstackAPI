edaf8a7ac74049ade8fd7a540ad70ca7c639679f
Anil <anilbca1996@gmail.com>
2025-08-24 14:47:15 +0530

    updated code

--- SSFullstackAPI/Program.cs
+++ SSFullstackAPI/Program.cs
@@ -8,6 +8,24 @@ using SSFullstackAPI.Data;
 var builder = WebApplication.CreateBuilder(args);
 var awsOptions = builder.Configuration.GetSection("AWS");
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//sql, mysql, postgresql, oracle
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon")));
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(MySqlConnection, ServerVersion.AutoDetect(MySqlCon)));
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseOracle(builder.Configuration.GetConnectionString("OracleCon")));
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(PostgresCon));
+
+// Mongodb
+builder.Services.AddSingleton<IMongoClient>(sp => { return new MongoClient(builder.Configuration.GetConnectionString("MongoCon")); });
+builder.Services.AddSingleton(sp =>
+{
+    var client = sp.GetRequiredService<IMongoClient>();
+    return client.GetDatabase(builder.Configuration.GetValue<string>("MongoDatabase"));
+});
+
 // Register DynamoDB client
 builder.Services.AddSingleton<IAmazonDynamoDB>(sp =>
 {
@@ -21,27 +39,7 @@ builder.Services.AddSingleton<IAmazonDynamoDB>(sp =>
 // Register S3 client
 builder.Services.AddSingleton<IAmazonS3>(sp =>
 {
-    return new AmazonS3Client(
-        awsOptions["AccessKey"],
-        awsOptions["SecretKey"],
-        RegionEndpoint.GetBySystemName(awsOptions["Region"])
-    );
-});
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon")));
-//builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(MySqlConnection, ServerVersion.AutoDetect(MySqlCon)));
-//builder.Services.AddDbContext<AppDbContext>(options => options.UseOracle(builder.Configuration.GetConnectionString("OracleCon")));
-//builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(PostgresCon));
-
-builder.Services.AddSingleton<IMongoClient>(sp => { return new MongoClient(builder.Configuration.GetConnectionString("MongoCon")); });
-builder.Services.AddSingleton(sp =>
-{
-    var client = sp.GetRequiredService<IMongoClient>();
-    return client.GetDatabase(builder.Configuration.GetValue<string>("MongoDatabase"));
+    return new AmazonS3Client(awsOptions["AccessKey"], awsOptions["SecretKey"], RegionEndpoint.GetBySystemName(awsOptions["Region"]));
 });
 
 builder.Services.AddCors(options =>
